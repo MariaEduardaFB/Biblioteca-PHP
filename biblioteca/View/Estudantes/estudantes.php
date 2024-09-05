2db153e9ee05037298d95aa5d7f7b85e705f81a5
Werbetonn <115298840+Werbetonn@users.noreply.github.com>
2024-09-05 10:29:21 -0300

    Add files via upload

---
+++ biblioteca/View/Estudantes/estudantes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudantes Cadastrados</title>
+    <link rel="stylesheet" href="estudantes_css.css">
+</head>
+<body>
+    <header>
+        <h3>Gerenciamento de Biblioteca</h3>
+
+        <nav>
+            <a href="../View/index.php">Início</a>
+            <a href="../Livros/livros.php">Livros</a>
+            <a href="../Estudantes/estudantes.php">Estudantes</a>
+            <a href="../Emprestimos/emprestimos.php">Empréstimos</a>
+        </nav>
+
+    </header>
+
+    <main>
+        <h2>Estudantes Cadastrados no Sistema</h2>
+        <ul>
+            <li>João da Silva</li>
+            <li>Maria Oliveira</li>
+            <li>Lucas Santos</li>
+            <li>Ana Souza</li>
+            <li>Carlos Pereira</li>
+        </ul>
+    </main>
+
+    <footer>
+        <p>&copy; Desenvolvido por Eduarda Batista, Vicencya Nayara e Werbeton da Silva.</p>
+    </footer>
+</body>
+</html>
---
+++ biblioteca/View/Livros/livros.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livros Disponíveis</title>
+    <link rel="stylesheet" href="livros_css.css">
+</head>
+<body>
+    <header>
+        <h3>Gerenciamento de Biblioteca</h3>
+
+        <nav>
+            <a href="../View/index.php">Início</a>
+            <a href="../Livros/livros.php">Livros</a>
+            <a href="../Estudantes/estudantes.php">Estudantes</a>
+            <a href="../Emprestimos/emprestimos.php">Empréstimos</a>
+        </nav>
+
+    </header>
+
+    <main>
+        <h2>Livros Disponíveis na Biblioteca</h2>
+        <ul>
+            <li>Harry Potter e a Pedra Filosofal</li>
+            <li>O Senhor dos Anéis: A Sociedade do Anel</li>
+            <li>1984</li>
+            <li>O Pequeno Príncipe</li>
+            <li>Orgulho e Preconceito</li>
+        </ul>
+    </main>
+    
+    <footer>
+        <p>&copy; Desenvolvido por Eduarda Batista, Vicencya Nayara e Werbeton da Silva.</p>
+    </footer>
+</body>
+</html>
